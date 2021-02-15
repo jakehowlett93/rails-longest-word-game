d10153176355449dca64265dab2a3b533618ee09
jake howlett <jake.howlett93@gmail.com>
2021-02-15 16:15:04 +0000

    improved logic for the score action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,21 +7,26 @@ class GamesController < ApplicationController
   end
 
   def score
-    @result = "correct"
     @letters = params[:letters].split(" ")
     @word = params[:word].upcase
+    @result = "Congratulations #{@word} is a valid English word!"
+    valid_letters = true
 
     @word.each_char.with_index do |char, index|
       if @letters.include? char
-        @letters.slice(index, 1)
+        @letters.slice!(index, 1)
       else
-        @result = "Invalid letters"
+        @result = "#{params[:letters]} does not contain #{@word}"
+        valid_letters = false
+        break
       end
-      url ="https://wagon-dictionary.herokuapp.com/#{@word}"
-      json_string = open(url).read
-      json_hash = JSON.parse(json_string)
-      unless json_hash["found"]
-        @result = "Word not in dictionary"
+      if valid_letters
+        url ="https://wagon-dictionary.herokuapp.com/#{@word}"
+        json_string = open(url).read
+        json_hash = JSON.parse(json_string)
+        unless json_hash["found"]
+          @result = "#{@word} is not in the English dictionary"
+        end
       end
     end
   end
